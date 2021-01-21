56065ec54a62b15a4217ea1d0210271d95285074
shenshan <shenshanpku@gmail.com>
2021-01-21 00:11:23 -0800

    datajoint-dashboard as requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 from os import path
 
 here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='odor-meso-gui',
@@ -11,5 +13,5 @@ setup(
     author='Vathes',
     author_email='support@vathes.com',
     packages=find_packages(exclude=[]),
-    install_requires=['datajoint>=0.12', 'dash', 'dash-bootstrap-components'],
+    install_requires=requirements,
 )
